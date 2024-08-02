a8bb8d3d47f8590d81b0099b813a5eb4054073ed
MENG,Ke <104410162+mengke25@users.noreply.github.com>
2024-08-02 20:33:22 +0800

    Update runcode_in_cmd.py

--- script/runcode_in_cmd.py
+++ script/runcode_in_cmd.py
@@ -1,33 +1,33 @@
 import subprocess
 import psutil
 import time
+import json
+
+
+# 读取配置文件
+with open('config/config.json', 'r', encoding='utf-8') as config_file:
+    config = json.load(config_file)
+
+# 获取全局配置
+global_config = config["global"]
+root_path = global_config["root_path"]
+python_env = global_config["python_env"]
+
 
 # 定义所有需要执行的命令
 commands = [
-    "activate base && python D:\project\July2024_textOpenAI\script\\a1.py",
-    "activate base && python D:\project\July2024_textOpenAI\script\\a2.py",
-    "activate base && python D:\project\July2024_textOpenAI\script\\a3.py",
-    "activate base && python D:\project\July2024_textOpenAI\script\\a4.py",
-    "activate base && python D:\project\July2024_textOpenAI\script\\a5.py",
-    "activate base && python D:\project\July2024_textOpenAI\script\\a6.py",
-    "activate base && python D:\project\July2024_textOpenAI\script\\a7.py",
-    "activate base && python D:\project\July2024_textOpenAI\script\\a8.py",
-    "activate base && python D:\project\July2024_textOpenAI\script\\a9.py",
-    "activate base && python D:\project\July2024_textOpenAI\script\\a10.py"
+    f"activate {python_env} && python {root_path}\\script\\a1.py",
+    f"activate {python_env} && python {root_path}\\script\\a2.py",
+    f"activate {python_env} && python {root_path}\\script\\a3.py",
+    f"activate {python_env} && python {root_path}\\script\\a4.py",
+    f"activate {python_env} && python {root_path}\\script\\a5.py",
+    f"activate {python_env} && python {root_path}\\script\\a6.py",
+    f"activate {python_env} && python {root_path}\\script\\a7.py",
+    f"activate {python_env} && python {root_path}\\script\\a8.py",
+    f"activate {python_env} && python {root_path}\\script\\a9.py",
+    f"activate {python_env} && python {root_path}\\script\\a10.py"
 ]
 
-
-#---------------------------------------------------------------------------------------------------------------#
-## 直接在cmd中分别运行
-# for command in commands:
-#     subprocess.Popen(['cmd', '/c', 'start', 'cmd', '/k', command])
-# print("所有命令已启动")
-
-
-
-
-#---------------------------------------------------------------------------------------------------------------#
-## 在编译器中一起运行
 processes = []
 
 # 启动所有进程
@@ -41,6 +41,8 @@ while processes:
         if process.poll() is not None:  # 检查进程是否结束
             processes.remove(process)
             print(f"Process {process.pid} completed with return code {process.returncode}")
-    time.sleep(10)  # 每秒检查一次
+    time.sleep(10)  # 每10秒检查一次
 
 print("所有进程已完成")
+
+
