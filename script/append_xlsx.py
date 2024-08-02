2cc3b0e7e0b7ec455665b5784b2c93ba62c16037
MENG,Ke <104410162+mengke25@users.noreply.github.com>
2024-08-02 18:35:51 +0800

    Update append_xlsx.py

--- script/append_xlsx.py
+++ script/append_xlsx.py
@@ -1,15 +1,22 @@
 import pandas as pd
 import os
+import json
 
-# 定义文件路径
-input_dir = r'D:\project\July2024_textOpenAI\output'
-output_file_path = os.path.join(input_dir, 'a.xlsx')
+# 读取配置文件
+with open('config/config.json', 'r', encoding='utf-8') as config_file:
+    config = json.load(config_file)
+
+# 获取全局配置
+global_config = config["global"]
+root_path = global_config["root_path"]
+output_dir = os.path.join(root_path, 'output')
+output_file_path = os.path.join(output_dir, 'a.xlsx')
 
 # 定义合并文件的数量
 Num_file = 10  # 你可以根据需要更改这个值
 
 # 读取第一个文件以获取标题行
-df = pd.read_excel(os.path.join(input_dir, 'a1.xlsx'))
+df = pd.read_excel(os.path.join(output_dir, 'a1.xlsx'))
 header = df.iloc[0].to_frame().T
 
 # 合并数据
@@ -17,7 +24,7 @@ data_frames = [df[1:]]  # 去除标题行
 
 # 读取剩余文件并合并
 for i in range(2, Num_file + 1):
-    file_path = os.path.join(input_dir, f'a{i}.xlsx')
+    file_path = os.path.join(output_dir, f'a{i}.xlsx')
     df = pd.read_excel(file_path)
     data_frames.append(df[1:])  # 去除标题行
 
